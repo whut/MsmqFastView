3029032fc7b1dbc2af08b98593a45bb47900ebc2
Jakub Berezanski <kuba@berezanscy.pl>
2012-09-09 16:32:23 +0200

    Displaying of unhandled exceptions for easier diagnostics.

--- MsmqFastView/App.xaml.cs
+++ MsmqFastView/App.xaml.cs
@@ -1,8 +1,27 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
 
 namespace MsmqFastView
 {
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            AppDomain.CurrentDomain.UnhandledException += OnUnhandledException;
+        }
+
+        private void OnUnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            MessageBox.Show(
+                string.Concat(
+                    "An unexpected error has occured. Please notify the developers.",
+                    Environment.NewLine,
+                    Environment.NewLine,
+                    "Details:",
+                    Environment.NewLine,
+                    e.ExceptionObject.ToString()), 
+                "Unhandled exception");
+        }
     }
 }
